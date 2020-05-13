0c673162daa61901f15945721d3cb466fda76a8c
Arina Busygina <thr3@mail.ru>
2020-05-13 18:11:55 +0300

    Add 5 task

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -17,22 +17,42 @@ function calculateTriangleSquare() {
 }
 
 
-function reverseWord(){
+function reverseWord() {
     const word = prompt('Введите слово для реверсии', '');
 
-    if(word){
+    if (word) {
         alert(word.split('').reverse().join(''));
     }
 }
 
-function removeMultipleSpaces(){
+function removeMultipleSpaces() {
     const word = prompt('Введите предложение', '');
 
-    if(word){
+    if (word) {
         alert(word.replace(/\s{2,}/g, ' ').trim());
     }
+}
+
+function countLetters() {
+    const word = prompt('Введите предложение', '');
+    const letters = word.toLowerCase().replace(/[^[a-z]]*/g, '');
+
+    const vowels = ['a', 'o', 'u', 'y', 'e', 'i'];
+    let vowelsCount = 0;
+    let match;
+
+    vowels.forEach((v) => {
+        match = letters.match(new RegExp(v, 'g'))
+
+        if (match) {
+            vowelsCount += match.length;
+        }
+    });
+
+    alert(`Гласных: ${vowelsCount}, согласных: ${letters.length - vowelsCount}`);
 }
 
 // calculateTriangleSquare();
 // reverseWord();
 // removeMultipleSpaces()
+// countLetters();
