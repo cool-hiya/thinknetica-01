df50e13f363edc20df424b8f228d2784070571d7
Arina Busygina <thr3@mail.ru>
2020-05-13 17:24:37 +0300

    Add first task

---
+++ js/lesson-1.js
@@ -0,0 +1,19 @@
+function calculateTriangleSquare() {
+    let width;
+
+    do {
+        width = prompt('Расчет площади правильного треугольника \nПожалуйста, введите длину стороны')
+
+        if (width) {
+            if (parseInt(width)) {
+                alert(`Площадь правильного треугольника со стороной ${width} будет ${(Math.pow(width, 2) * Math.sqrt(3) / 4).toFixed(2)}`);
+            } else if (width !== null) {
+                alert('Ошибка! Длина должна быть числом');
+                width = '';
+            }
+        }
+
+    } while (width !== null && !width) {}
+}
+
+//calculateTriangleSquare();
