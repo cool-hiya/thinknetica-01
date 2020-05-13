d6615f10ff2fe0200939a8ebfd31ac371b6c4a15
Arina Busygina <thr3@mail.ru>
2020-05-13 18:29:51 +0300

    Add 4 task

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -52,7 +52,28 @@ function countLetters() {
     alert(`Гласных: ${vowelsCount}, согласных: ${letters.length - vowelsCount}`);
 }
 
+
+function getArithmeticMean() {
+    let number;
+    let arr = [];
+    let sum = 0;
+    let result;
+
+    do {
+        number = prompt('Введите число', '');
+        if (number) {
+            arr.push(+number);
+            sum = arr.reduce((a, b) => a + b, 0) ;
+            result = sum / arr.length;
+            console.log(`Предварительный итог: ${result}`);
+        }
+    } while (number) {}
+
+alert(`Сумма чисел: ${sum} \nКоличество чисел: ${arr.length} \nСреднее арифметическое: ${result}`);
+}
+
 // calculateTriangleSquare();
 // reverseWord();
 // removeMultipleSpaces()
 // countLetters();
+getArithmeticMean();
