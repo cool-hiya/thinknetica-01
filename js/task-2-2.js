3662510ddd3017eb7648cd1988501fe59138ea30
Arina Busygina <thr3@mail.ru>
2020-05-14 21:29:33 +0300

    Add 2 task

--- js/task-2-2.js
+++ js/task-2-2.js
@@ -1,1 +1,8 @@
-console.log('test');
+function encodeId() {
+    const data = [1588312016, 58, 8, 5553663];
+    let result = data.map(d => Number(d).toString(16)).join('');
+
+    alert(result);
+}
+
+encodeId();
