4f4c443e10ef071819ef5b1eb811b56edc00a278
Arina Busygina <thr3@mail.ru>
2020-05-15 21:15:39 +0300

    Updated 3 task

--- js/task-2-3-extra.js
+++ js/task-2-3-extra.js
@@ -1,11 +1,17 @@
-function clipText() {
-    let text = 'Многие думают, что Lorem Ipsum - взятый с потолка псевдо-латинский набор слов, но это не совсем так. Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney, штат Вирджиния, взял одно из самых странных слов в Lorem Ipsum';
+function formatText() {
+    const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed semper nulla. Nulla facilisi. Nulla mi massa, porttitor vel est non, finibus faucibus mi. Ut id erat venenatis, suscipit tellus ac, tempus mi. Donec tristique efficitur ipsum nec facilisis. Integer commodo, felis eu aliquam mattis, lacus nulla ornare mauris, sed accumsan purus purus id nisl. Proin eu blandit nisl. Aliquam a gravida sem, eu eleifend tellus. Praesent venenatis elit id mauris accumsan, nec rhoncus orci vehicula. In congue urna vel tellus ullamcorper, in pretium quam faucibus. Donec quam leo, mattis a magna vel, hendrerit rutrum nisi. Pellentesque id ornare mauris. Sed volutpat, ex vehicula dignissim gravida, mi ante eleifend nulla, vel condimentum justo nulla sed libero. Quisque a arcu et dolor fringilla volutpat quis eu sem.';
+    const arr = text.split('');
     const max = 80;
+    let index = max;
+    let counter = 0;
 
-    if (text.length > max) {
-        text = text.substr(0, max - 3) + '...';
+    while (index <= text.length + counter) {
+        arr.splice(index, 0, '\n');
+        index += max + 1;
+        counter++;
     }
-    console.log(text);
+
+    alert(arr.join(''));
 }
 
-clipText();
+formatText();
