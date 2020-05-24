11c77a7b34e3b4925d7a3e3a7b9f46d8e4b56149
Arina Busygina <thr3@mail.ru>
2020-05-24 20:08:58 +0300

    Add 1 task

---
+++ dom/task-1/script.js
@@ -0,0 +1,47 @@
+function domTree1(el) {
+    if (!el) {
+        return;
+    }
+
+    if (!el.children) {
+        return;
+    } else {
+        [...el.children].forEach((el) => {
+            console.log(el.tagName);
+            domTree1(el);
+        });
+    }
+}
+
+function domTree2(el) {
+    if (!el) {
+        return;
+    }
+
+    if (!el.children) {
+        return;
+    } else {
+        for (let i = 0; i < el.children.length; i++) {
+            const child = el.children[i];
+            console.log(child.tagName);
+            domTree2(child);
+        }
+    }
+}
+
+function domTree3(el) {
+    if (!el) {
+        return;
+    }
+
+    if (!el.children) {
+        return;
+    } else {
+        for (let child of el.children) {
+            console.log(child.tagName);
+            domTree3(child);
+        }
+    }
+}
+
+console.log(domTree3(document));
