8519181ffee2ead27f43030783dc785de446441e
Arina Busygina <thr3@mail.ru>
2020-05-25 17:56:03 +0300

    Add 3 way for 1 task

--- dom/task-1/script.js
+++ dom/task-1/script.js
@@ -21,4 +21,20 @@ function domTree2(node, deep = 0) {
     }
 }
 
-domTree2(document);
+function domTree3(el, deep = 0) {
+    if (!el) {
+        return;
+    }
+
+    if (el.children.length === 0) {
+        return;
+    } else {
+        for (let i = 0; i < el.children.length; i++) {
+            const child = el.children[i];
+            console.log('  '.repeat(deep) + child.tagName);
+            domTree3(child, deep + 1);
+        }
+    }
+}
+
+domTree3(document);
