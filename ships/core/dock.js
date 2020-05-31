8bb19924e3966dc5b067cd8ee0d9e6cc72825de3
Arina Busygina <thr3@mail.ru>
2020-05-31 17:16:26 +0300

    Add 1 task

---
+++ ships/core/dock.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * 
+ * @param {Position} position 
+ * @param {Ship[]} ships 
+ */
+function Dock(position = new Position(0, 0), ships = []) {
+    this.position = position;
+    this.ships = ships;
+
+    /**
+     * @param {Ship} ship
+     */
+    this.moor = function (ship) {
+        if (this._hasShip(ship)) {
+            throw new Error('Already moored');
+        }
+
+        if ((this.position.x !== ship.position.x) || (this.position.y !== ship.position.y)) {
+            throw new Error('Move to dock to moor');
+        }
+
+        ship.dropAnchor();
+        this.ships.push(ship);
+    }
+
+    /**
+     * @param {Ship} ship
+     */
+    this.unmoor = function (ship) {
+        if (!this._hasShip(ship)) {
+            throw new Error('Not in the dock');
+        }
+
+        const index = this.ships.findIndex(s => ship.name === s.name);
+        this.ships.splice(index, 1);
+        ship.riseAnchor();
+    }
+
+    /**
+     * @param {Ship} ship
+     */
+    this._hasShip = function (ship) {
+        return !!this.ships.find(s => ship.name === s.name);
+    }
+}
---
+++ ships/shared/position.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @param {number} x 
+ * @param {number} y 
+ */
+function Position(x, y) {
+    this.x = x;
+    this.y = y;
+}
